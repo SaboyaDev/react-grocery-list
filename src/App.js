603dd04a3e187c32b44da2f909eaac76edb61d9f
Jose L Saboya <saboyadev@gmail.com>
2022-12-03 03:44:29 -0500

    Created this new repo from a previous project, will continue to build on this new repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/header.component'
+import AddItem from './components/add-item/add-item.component'
+import SearchItem from './components/search-item/search-item.component'
+import Content from './components/content/content.component'
+import Footer from './components/footer/footer.component'
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Get Local Storage List
+  const getShoppingList = localStorage.getItem('shopping-list')
+	// State
+	const [items, setItems] = useState(JSON.parse(getShoppingList) || [])
+	const [newItem, setNewItem] = useState('')
+	const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('shopping-list', JSON.stringify(items))
+  }, [items])
+
+	const addItem = item => {
+		const id = items.length ? items[items.length - 1].id + 1 : 1
+		const myNewItem = { id, checked: false, item }
+		const listItems = [...items, myNewItem]
+		setItems(listItems)
+	}
+	const handleChange = id => {
+		const listItems = items.map(item =>
+			item.id === id ? { ...item, checked: !item.checked } : item
+		)
+		setItems(listItems)
+	}
+	const handleDelete = id => {
+		const listItems = items.filter(item => item.id !== id)
+		setItems(listItems)
+	}
+	const handleSubmit = e => {
+		e.preventDefault()
+		addItem(newItem)
+		setNewItem('')
+	}
+
+	return (
+		<div className='App'>
+			<Header title='Grocery List' />
+			<AddItem
+				newItem={newItem}
+				setNewItem={setNewItem}
+				handleSubmit={handleSubmit}
+			/>
+			<SearchItem
+				search={search}
+				setSearch={setSearch}
+			/>
+			<Content
+				items={items.filter(item =>
+					item.item.toLowerCase().includes(search.toLocaleLowerCase())
+				)}
+				handleChange={handleChange}
+				handleDelete={handleDelete}
+			/>
+			<Footer length={items.length} />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,12 @@
+import './footer.styles.css'
+
+const Footer = ({ length }) => {
+	return (
+		<footer>
+			<p>
+				{length} List {length === 1 ? 'Item' : 'Items'}
+			</p>
+		</footer>
+	)
+}
+export default Footer
